252c293c221fc394d731c9a5da3e547aba7330a9
Nicolas Magnani <nicolas.mmagnani@senacsp.edu.br>
2025-10-15 21:41:29 -0300

    Create informaçoes.py

---
+++ informaçoes.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome: ")
+ano_nascimento = int(input("Digite seu ano de nascimento: "))
+idade = int(input("Digite sua idade: "))
+cidade = input("Digite o nome da sua cidade: ")
+
+print(f"Olá, {nome}! Você nasceu em {ano_nascimento}, tem {idade} anos e mora em {cidade}.")
